1e01ea452e58532886c57c814fe6bf7e478b03e7
tisoda <tisoda@gmai.com>
2025-10-08 20:51:48 +0700

    Update middleware configuration to enhance security and CORS settings. Implement content security policies and specify allowed origins, headers, and methods for CORS.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,39 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://admin.tisoda.com',
+            'https://market-assets.strapi.io',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://admin.tisoda.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://admin.tisoda.com', 'http://localhost:*', 'https://tisoda.com', '*'],
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
