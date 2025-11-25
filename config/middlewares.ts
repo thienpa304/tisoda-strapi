e5f90f71b7cb4365b379f0d3553d854e7c9ac79a
Thien <thienpham8817@gmail.com>
2025-11-25 15:47:18 +0700

    Update CORS configuration to include 'https://www.tisoda.com' in allowed origins for improved access control

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -29,7 +29,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://admin.tisoda.com', 'https://tisoda.com', 'https://dev.tisoda.com', 'http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000'],
+      origin: ['https://admin.tisoda.com', 'https://tisoda.com', 'https://dev.tisoda.com', 'https://www.tisoda.com', 'http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000'],
       headers: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       credentials: true,
